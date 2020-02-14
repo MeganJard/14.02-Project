2dc326b0eb5ad08896578cb5f97367c154b72278
MeganJard <57149105+MeganJard@users.noreply.github.com>
2020-02-14 17:09:22 +0300

    readyFirstTask

--- scratch.py
+++ scratch.py
@@ -1,28 +1,66 @@
+import pygame
 import requests
+from io import BytesIO
+from PIL import Image
+import sys, os
+pygame.init()
 
-def get_pic(geocode='Москва, Кремль', spn='0.005, 0.005'):
 
-    geocode = geocode
-    spn = spn
-
-    geoserver = 'https://geocode-maps.yandex.ru/1.x/'
-
-    geoparams = {
-        'geocode':geocode,
-        'spn': spn,
-        'apikey':'40d1649f-0493-4b70-98ba-98533de7710b'
-    }
-    point = requests.get(geoserver, params=geoparams).json()["response"]["GeoObjectCollectio"
-                                  "n"]["featureMember"][0]["GeoObject"]['Point']['po'
-                                                                                 's']
-
+def get_pic(coords='39 52', spn='0.005 0.005'):
     picserver = 'https://static-maps.yandex.ru/1.x/'
 
     picparams = {
-    'l':'map',
-    'll': ','.join(point.split()),
-    'spn': ','.join(spn.split())
+        'l': 'map',
+        'll': ','.join(coords.split()),
+        'spn': ','.join(spn.split())
     }
 
     response = requests.get(picserver, params=picparams).content
     return response
+
+
+class Paint(pygame.sprite.Sprite):
+    def __init__(self, image, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = image
+
+map_file = "map.png"
+try:
+    with open(map_file, "wb") as file:
+        file.write(get_pic())
+except IOError as ex:
+    print("Ошибка записи временного файла:", ex)
+    sys.exit(2)
+
+    # Инициализируем pygame
+pygame.init()
+screen = pygame.display.set_mode((600, 450))
+# Рисуем картинку, загружаемую из только что созданного файла.
+screen.blit(pygame.image.load(map_file), (0, 0))
+# Переключаем экран и ждем закрытия окна.
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+
+pygame.quit()
+    # Удаляем за собой файл с изображением.
+os.remove(map_file)
+
+FPS = 50
+screen = pygame.display.set_mode((500, 500))
+screen.fill((0, 0, 0))
+pygame.display.flip()
+running = True
+clock = pygame.time.Clock()
+img = pygame.image.load('map.img')
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            break
+    pygame.display.flip()
+
+    screen.blit(img, (0, 0))
+    clock.tick(30)
+
+pygame.quit()
