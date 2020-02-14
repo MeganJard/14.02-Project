69fd1d0b3d0de1ae40349fd2ebf69c2d3f547741
MeganJard <57149105+MeganJard@users.noreply.github.com>
2020-02-14 16:25:25 +0300

    Подгрузка картинки

---
+++ scratch.py
@@ -0,0 +1,28 @@
+import requests
+
+def get_pic(geocode='Москва, Кремль', spn='0.005, 0.005'):
+
+    geocode = input('Введите адрес')
+    spn = input('Введите масштаб')
+
+    geoserver = 'https://geocode-maps.yandex.ru/1.x/'
+
+    geoparams = {
+        'geocode':geocode,
+        'spn': spn,
+        'apikey':'40d1649f-0493-4b70-98ba-98533de7710b'
+    }
+    point = requests.get(geoserver, params=geoparams).json()["response"]["GeoObjectCollectio"
+                                  "n"]["featureMember"][0]["GeoObject"]['Point']['po'
+                                                                                 's']
+
+    picserver = 'https://static-maps.yandex.ru/1.x/'
+
+    picparams = {
+    'l':'map',
+    'll': ','.join(point.split()),
+    'spn': ','.join(spn.split())
+    }
+
+    response = requests.get(picserver, params=picparams).content
+    return response
