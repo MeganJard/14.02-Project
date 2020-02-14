83d48b9da3ba7fc265a1eb3e2d2ab978eeb2030b
TheLostDesu <57669330+TheLostDesu@users.noreply.github.com>
2020-02-14 19:19:02 +0300

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,62 +1,53 @@
-import requests
 import pygame
+import requests
+from io import BytesIO
+from PIL import Image
+import sys, os
 
 
 pygame.init()
 
 
-def get_pic(geocode='Москва, Кремль', spn='0.005, 0.005'):
-
-    geocode = geocode
-    spn = spn
-
-    geoserver = 'https://geocode-maps.yandex.ru/1.x/'
-
-    geoparams = {
-        'geocode':geocode,
-        'spn': spn,
-        'apikey':'40d1649f-0493-4b70-98ba-98533de7710b'
-    }
-    point = requests.get(geoserver, params=geoparams).json()["response"]["GeoObjectCollectio"
-                                  "n"]["featureMember"][0]["GeoObject"]['Point']['po'
-                                                                                 's']
-
+def get_pic(coords='39 52', spn='0.005 0.005'):
     picserver = 'https://static-maps.yandex.ru/1.x/'
-
     picparams = {
-    'l':'map',
-    'll': ','.join(point.split()),
-    'spn': ','.join(spn.split())
+        'l': 'map',
+        'll': ','.join(coords.split()),
+        'spn': ','.join(spn.split())
     }
-
     response = requests.get(picserver, params=picparams).content
     return response
 
 
-class Paint(pygame.sprite.Sprite):
-    def __init__(self, image, location):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = image
-        self.rect = self.image.get_rect()
-        self.rect.left, self.rect.top = location
+map_file = "map.png"
+try:
+    with open(map_file, "wb") as file:
+        file.write(get_pic())
+except IOError as ex:
+    print("Ошибка записи временного файла:", ex)
+    sys.exit(2)
 
 
+screen = pygame.display.set_mode((600, 450))
+screen.blit(pygame.image.load(map_file), (0, 0))
+pygame.display.flip()
+while pygame.event.wait().type != pygame.QUIT:
+    pass
+os.remove(map_file)
 FPS = 50
 screen = pygame.display.set_mode((500, 500))
 screen.fill((0, 0, 0))
 pygame.display.flip()
 running = True
 clock = pygame.time.Clock()
-
+img = pygame.image.load('map.img')
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
             break
     pygame.display.flip()
-    p = Paint(get_pic(), [0, 0])
-    screen.blit(p.image, p.rect)
+
+    screen.blit(img, (0, 0))
     clock.tick(30)
-
-
 pygame.quit()
